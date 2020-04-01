ec4c115cdbb53d99be37124ea1cf9f8647452d4e
呆奇士 チ <tsaiyuchi88@gmail.com>
2020-04-01 13:56:25 +0800

    update

--- vssol/BoxTransferTest/ViewUc/UcMaskRobot.cs
+++ vssol/BoxTransferTest/ViewUc/UcMaskRobot.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaskTool.TestMy.Device;
 
 namespace BoxTransferTest.ViewUc
 {
     public partial class UcMaskRobot : UserControl
     {
+        RobotHandler robotHandler;
+
+
         public UcMaskRobot()
         {
             InitializeComponent();
@@ -30,9 +34,37 @@ namespace BoxTransferTest.ViewUc
         private void btnStart_Click(object sender, EventArgs e)
         {
 
+
+
         }
 
         private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UcMaskRobot_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            robotHandler = new RobotHandler();
+            robotHandler.ldd.RobotIp = "192.168.0.50";
+            int connectRes = robotHandler.ConnectIfNO();
+            if (robotHandler != null)
+            {
+                robotHandler.ldd.StopProgram();
+                robotHandler.ldd.AlarmReset();
+            }
+            robotHandler.ldd.ExecutePNS("PNS0101");
+
+            robotHandler.getCurrentPOS();
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
         {
 
         }
