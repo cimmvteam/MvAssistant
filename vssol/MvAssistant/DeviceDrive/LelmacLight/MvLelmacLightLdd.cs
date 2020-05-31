ca4191ac3cd4f0b77cf26e7f264e20269a6f605e
呆奇士 チ <tsaiyuchi88@gmail.com>
2020-05-31 21:47:52 +0800

    add light ldd

---
+++ vssol/MvAssistant/DeviceDrive/LelmacLight/MvLelmacLightLdd.cs
@@ -0,0 +1,36 @@
+﻿using CToolkit.v1_1.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvAssistant.DeviceDrive.LelmacLight
+{
+    public class MvLelmacLightLdd
+    {
+
+        public CtkNonStopTcpClient TcpClient;
+
+
+
+        public MvLelmacLightLdd()
+        {
+
+        }
+
+
+        public int ConnectIfNo()
+        {
+            this.TcpClient = new CtkNonStopTcpClient();
+
+
+            return 0;
+        }
+
+
+
+
+
+    }
+}
