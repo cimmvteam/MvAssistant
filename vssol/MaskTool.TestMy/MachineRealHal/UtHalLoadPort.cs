4d378127e76c33040fce72d4cc108334533516a5
King Liu <64719094+twmland@users.noreply.github.com>
2020-06-16 17:39:12 +0800

    Loadport Event & alarm Handler

--- vssol/MaskTool.TestMy/MachineRealHal/UtHalLoadPort.cs
+++ vssol/MaskTool.TestMy/MachineRealHal/UtHalLoadPort.cs
@@ -17,11 +17,11 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         MvGudengLoadPortLdd ldd = new MvGudengLoadPortLdd();
       public UtHalLoadPort()
       {
-         //  LoadPort1 =  ldd.CreateLoadPort("192.168.0.11", 2013, 1);
+         
             LoadPort1 = ldd.CreateLoadPort("192.168.0.20", 1024, 1);
             LoadPort2 = ldd.CreateLoadPort("192.168.0.21", 1024, 2);
-            LoadPort1.ListenServer();
-            LoadPort2.ListenServer();
+            LoadPort1.StartListenServerThread();
+            LoadPort2.StartListenServerThread();
         }
 
 
@@ -29,39 +29,101 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         [TestMethod]
          public void TestCommandInitialRequest()
         {
-           // LoadPort2.CommandAlarmReset();
-           // LoadPort2.CommandInitialRequest();
-           // return;
-            // ~109,AlarmReset@
-            LoadPort1.CommandAlarmReset(); //~013,AlarmResetSuccess@
-           
-            //~112,InitialRequest@
-            LoadPort1.CommandInitialRequest();//[~018,LoadportStatus,2@][~202,VacuumAbnormality@~018,LoadportStatus,3@]
-            // 沒有收到 019,InitialComplete()
+            /**
+              // LoadPort2.CommandAlarmReset();
+             // LoadPort2.CommandInitialRequest();
+             // return;
+              // ~109,AlarmReset@
+              LoadPort1.CommandAlarmReset(); //~013,AlarmResetSuccess@
 
+              //~112,InitialRequest@
+              LoadPort1.CommandInitialRequest();//[~018,LoadportStatus,2@][~202,VacuumAbnormality@~018,LoadportStatus,3@]
+              // 沒有收到 019,InitialComplete()
+              */
+            EventHandler InitialCompleteHandler = (sender, e)=>{  };
+            EventHandler InitialUnCompleteHandler = (sender, e) =>{ };
+
+            LoadPort1.ResetInitialOnCompleteHandler();
+            LoadPort1.ResetOnInitialUnCompleteHandler();
+            LoadPort1.OnInitialCompleteHandler += InitialCompleteHandler;
+            LoadPort1.OnInitialUnCompleteHandler += InitialUnCompleteHandler;
+
+            LoadPort1.CommandInitialRequest();
         }
 
         /// <summary>(100)</summary>
         [TestMethod]
         public void TestCommandDockRequest()
         {
-
+            /**
            LoadPort1.CommandAlarmReset();  // ~013,AlarmResetSuccess@
            LoadPort1.CommandInitialRequest();//[~018,LoadportStatus,2@],[~202,VacuumAbnormality@~018,LoadportStatus,3@]
                                               // LoadPort1.CommandAlarmReset();
 
             // ~100,DockRequest@
             LoadPort1.CommandDockRequest();// ~015,ExecuteInitialFirst@  有沒有(沒有實際的 POD 無法測)
+            */
+            EventHandler ExecuteAlarmResetFirstHandler = (sender, e)=> {      };
+            EventHandler AlarmResetSuccessHandler = (sender, e) =>{  };
+            EventHandler AlarmResetFailHandler = (sender, e) =>{ };
+            EventHandler ExecuteInitialFirstHandler = (sender, e)=>{};
+            EventHandler InitialCompleteHandler = (sender, e) =>{};
+            EventHandler InitialUnCompleteHandler = (sender, e) => { };
+            EventHandler DockPODStartHandler = (sender, e) =>{};
 
+            LoadPort1.ResetExecuteOnAlarmResetFirstHandler();
+            LoadPort1.OnExecuteAlarmResetFirstHandler += ExecuteAlarmResetFirstHandler;
+            LoadPort1.ResetOnAlarmResetSuccessHandler();
+            LoadPort1.OnAlarmResetSuccessHandler += AlarmResetSuccessHandler;
+            LoadPort1.ResetOnAlarmResetFailHandler();
+            LoadPort1.OnAlarmResetFailHandler += AlarmResetFailHandler;
+            LoadPort1.ResetOnExecuteInitialFirstHandler();
+            LoadPort1.OnExecuteInitialFirstHandler += ExecuteInitialFirstHandler;
+            LoadPort1.ResetInitialOnCompleteHandler();
+            LoadPort1.OnExecuteInitialFirstHandler += ExecuteInitialFirstHandler;
+            LoadPort1.ResetOnInitialUnCompleteHandler();
+            LoadPort1.OnInitialUnCompleteHandler += InitialUnCompleteHandler;
+            LoadPort1.ResetInitialOnCompleteHandler();
+            LoadPort1.OnInitialCompleteHandler += InitialCompleteHandler;
+            LoadPort1.ResetDockOnPODStartHandler();
+            LoadPort1.OnDockPODStartHandler += DockPODStartHandler;
 
+            LoadPort1.CommandDockRequest();
         }
+
+        /// <summary>(101)</summary>
         [TestMethod]
         public void TestCommandUndockRequest()
         {
+            /**
             LoadPort1.CommandAlarmReset();//~013,AlarmResetSuccess@
             LoadPort1.CommandInitialRequest();
             //~101,UndockRequest@
             LoadPort1.CommandUndockRequest(); //~020,MustInAutoMode@(缺)(若是和前面指令一起連續執行則沒有回覆)
+            */
+            EventHandler ExecuteAlarmResetFirstHandler = (sender, e) => { };
+            EventHandler AlarmResetSuccessHandler = (sender, e) => { };
+            EventHandler AlarmResetFailHandler = (sender, e) => { };
+            EventHandler ExecuteInitialFirstHandler = (sender, e) => { };
+            EventHandler InitialCompleteHandler = (sender, e) => { };
+            EventHandler InitialUnCompleteHandler = (sender, e) => { };
+
+            LoadPort1.ResetExecuteOnAlarmResetFirstHandler();
+            LoadPort1.OnExecuteAlarmResetFirstHandler += ExecuteAlarmResetFirstHandler;
+            LoadPort1.ResetOnAlarmResetSuccessHandler();
+            LoadPort1.OnAlarmResetSuccessHandler += AlarmResetSuccessHandler;
+            LoadPort1.ResetOnAlarmResetFailHandler();
+            LoadPort1.OnAlarmResetFailHandler += AlarmResetFailHandler;
+            LoadPort1.ResetOnExecuteInitialFirstHandler();
+            LoadPort1.OnExecuteInitialFirstHandler += ExecuteInitialFirstHandler;
+            LoadPort1.ResetInitialOnCompleteHandler();
+            LoadPort1.OnExecuteInitialFirstHandler += ExecuteInitialFirstHandler;
+            LoadPort1.ResetOnInitialUnCompleteHandler();
+            LoadPort1.OnInitialUnCompleteHandler += InitialUnCompleteHandler;
+            LoadPort1.ResetInitialOnCompleteHandler();
+            LoadPort1.OnInitialCompleteHandler += InitialCompleteHandler;
+
+            LoadPort1.CommandUndockRequest();
         }
 
        
@@ -70,8 +132,8 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         public void TestCommandAskPlacementStatus()
         {
             //  ~102,AskPlacementStatus@
-            LoadPort1.CommandAskPlacementStatus();//~001,Placement,1@
-            LoadPort2.CommandAskPlacementStatus();//~001,Placement,0@
+           LoadPort1.CommandAskPlacementStatus();//~001,Placement,1@
+          //  LoadPort2.CommandAskPlacementStatus();//~001,Placement,0@
         }
 
         [TestMethod]
@@ -79,7 +141,7 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
            // ~103,AskPresentStatus@
             LoadPort1.CommandAskPresentStatus();//~002,Present,1@
-            LoadPort1.CommandAskPresentStatus();//~002,Present,0@
+           // LoadPort1.CommandAskPresentStatus();//~002,Present,0@
         }
 
         [TestMethod]
@@ -87,7 +149,7 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
             //~104,AskClamperStatus@
             LoadPort1.CommandAskClamperStatus();//[~003,Clamper,0@],[~006,ClamperLockComplete,0@]
-            LoadPort2.CommandAskClamperStatus();//[~003,Clamper,2@],[~006,ClamperLockComplete,1@]
+           // LoadPort2.CommandAskClamperStatus();//[~003,Clamper,2@],[~006,ClamperLockComplete,1@]
         }
 
         [TestMethod]
@@ -95,7 +157,7 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
             // ~105,AskRFIDStatus@
             LoadPort1.CommandAskRFIDStatus();//~004,RFID,1343833305251403@
-            LoadPort2.CommandAskRFIDStatus();// ~004,RFID,ERROR@
+           // LoadPort2.CommandAskRFIDStatus();// ~004,RFID,ERROR@
         }
 
         [TestMethod]
@@ -103,7 +165,7 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
             //~106,AskBarcodeStatus@
             LoadPort1.CommandAskBarcodeStatus();//~005,Barcode ID,0@
-            LoadPort2.CommandAskBarcodeStatus();//没有回覆
+         //   LoadPort2.CommandAskBarcodeStatus();//没有回覆
         }
 
         [TestMethod]
@@ -111,7 +173,7 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
             //~107,AskVacuumStatus@
             LoadPort1.CommandAskVacuumStatus();//~007,VacuumComplete,0@
-            LoadPort2.CommandAskVacuumStatus();//~007,VacuumComplete,0@
+          //  LoadPort2.CommandAskVacuumStatus();//~007,VacuumComplete,0@
         }
 
         [TestMethod]
@@ -119,43 +181,63 @@ namespace MvAssistant.Mac.TestMy.MachineRealHal
         {
             //~108,AskReticleExistStatus@
             LoadPort1.CommandAskReticleExistStatus();//~010,DockPODComplete_Empty@
-            LoadPort2.CommandAskReticleExistStatus();//~010,DockPODComplete_Empty@
+         //   LoadPort2.CommandAskReticleExistStatus();//~010,DockPODComplete_Empty@
         }
 
         [TestMethod]
-        
+
         public void TestCommandAlarmReset()
         {
-            //~109,AlarmReset@
-            LoadPort1.CommandAlarmReset();//~013,AlarmResetSuccess@
-            LoadPort2.CommandAlarmReset();//~013,AlarmResetSuccess@
+            /**
+             //~109,AlarmReset@
+             LoadPort1.CommandAlarmReset();//~013,AlarmResetSuccess@
+             LoadPort2.CommandAlarmReset();//~013,AlarmResetSuccess@
+            */
+        
+            LoadPort1.CommandAlarmReset();
         }
 
         [TestMethod]
         public void TestCommandAskStagePosition()
         {
-          
-           
-            //~110,AskStagePosition@
-            //LoadPort1.CommandAskStagePosition();// ~017,StagePosition,0@
-            LoadPort2.CommandAskStagePosition();// ~017,StagePosition,3@
+
+            /**
+             //~110,AskStagePosition@
+             //LoadPort2.CommandAskStagePosition();// ~017,StagePosition,0@
+             */
+
+         
+            // LoadPort1.ReleaseCommandCasadeMode(); ??
+            LoadPort1.CommandAskStagePosition();// ~017,StagePosition,3@
         }
 
         [TestMethod]
         public void TestCommandAskLoadportStatus()
         {
+            /**
             // ~111,AskLoadportStatus@
-            LoadPort1.CommandAskLoadportStatus();//~018,LoadportStatus,4
             LoadPort2.CommandAskLoadportStatus();//~018,LoadportStatus,1@
+            */
+
+        
+            // LoadPort1.ReleaseCommandCasadeMode(); ??
+            LoadPort1.CommandAskLoadportStatus();//~018,LoadportStatus,4
+          
         }
 
         [TestMethod]
         public void TestCommandManualClamperLock()
         {
-             //LoadPort2.CommandInitialRequest();
-            //~113,ManualClamperLock@
+            /**
+              //LoadPort2.CommandInitialRequest();
+              //~113,ManualClamperLock@
+              //LoadPort2.CommandManualClamperLock();//POD Problem
+              */
+
+    
+            // LoadPort1.ReleaseCommandCasadeMode(); ??
             LoadPort1.CommandManualClamperLock();//~003,Clamper,1@(第二次執行時沒有回覆)
-           LoadPort2.CommandManualClamperLock();//POD Problem
+           
         }
 
         [TestMethod]
