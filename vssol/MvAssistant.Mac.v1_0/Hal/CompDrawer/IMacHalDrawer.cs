3b2122a84bbc82270721c6c280a691956108f2c3
King Liu <ntutking@hotmail.com>
2020-06-23 09:46:38 +0800

    Declare  class drawer which implemet IMacHalDrawer

--- vssol/MvAssistant.Mac.v1_0/Hal/CompDrawer/IMacHalDrawer.cs
+++ vssol/MvAssistant.Mac.v1_0/Hal/CompDrawer/IMacHalDrawer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,33 @@ namespace MvAssistant.Mac.v1_0.Hal.CompDrawer
        
     }
 
-    
+    public class Drawer : IMacHalDrawer
+    {
+
+        public MvKjMachineDrawerLdd DrawerLdd { get; set; }
+       
+        public Drawer(int cabinetNO, string drawerNO, IPEndPoint deviceEndpoint, string localIp, IDictionary<int, bool?> portTable)
+        {
+            DrawerLdd = new MvKjMachineDrawerLdd(cabinetNO, drawerNO, deviceEndpoint, localIp, portTable); 
+        }
+        public int HalClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HalConnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HalIsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HalStop()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
