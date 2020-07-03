2aff6f8fcbbafe7c429ac1d18d0f9789fd1cfd5e
King Liu <ntutking@hotmail.com>
2020-07-03 10:37:49 +0800

    Define some class

---
+++ vssol/MvAssistant.Mac.v1_0/JSon/PositionInfoFile/BoxrobotTransferPathCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvAssistant.Mac.v1_0.JSon.PositionInfoFile
+{
+   public  enum BoxrobotTransferPathCategory
+    {
+        Get=0,
+        Put
+    }
+}
---
+++ vssol/MvAssistant.Mac.v1_0/JSon/PositionInfoFile/MaskrobotTransferPathFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvAssistant.Mac.v1_0.JSon.PositionInfoFile
+{
+    public  class MaskrobotTransferPathFile : BaserobotTransferPathFile
+    {
+    }
+}
