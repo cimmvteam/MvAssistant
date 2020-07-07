aa1e3b5d6697db46df05aad82a85e812def689c5
LingChengYeh <a1qaz123698745@gmail.com>
2020-07-07 17:17:25 +0800

    BT UI

--- vssol/MvAssistant.Mac.VerifyEqp/FrmTestUI.Designer.cs
+++ vssol/MvAssistant.Mac.VerifyEqp/FrmTestUI.Designer.cs
@@ -37,9 +37,18 @@
             this.GrpDrawerA = new System.Windows.Forms.GroupBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Lock = new System.Windows.Forms.Button();
+            this.Unlock = new System.Windows.Forms.Button();
+            this.BTGetOS = new System.Windows.Forms.Button();
+            this.BTPutOS = new System.Windows.Forms.Button();
+            this.lblBoxType = new System.Windows.Forms.Label();
+            this.txtBoxType = new System.Windows.Forms.TextBox();
+            this.BTPutDR_0204 = new System.Windows.Forms.Button();
+            this.BTGetDR_0204 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.txtBxIpA.SuspendLayout();
             this.GrpDrawer.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,19 +56,21 @@
             this.tabControl1.Controls.Add(this.txtBxIpA);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(16, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1567, 739);
+            this.tabControl1.Size = new System.Drawing.Size(2089, 924);
             this.tabControl1.TabIndex = 4;
             // 
             // txtBxIpA
             // 
             this.txtBxIpA.Controls.Add(this.GrpDrawer);
-            this.txtBxIpA.Location = new System.Drawing.Point(4, 22);
+            this.txtBxIpA.Location = new System.Drawing.Point(4, 25);
+            this.txtBxIpA.Margin = new System.Windows.Forms.Padding(4);
             this.txtBxIpA.Name = "txtBxIpA";
-            this.txtBxIpA.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.txtBxIpA.Size = new System.Drawing.Size(1559, 713);
+            this.txtBxIpA.Padding = new System.Windows.Forms.Padding(4);
+            this.txtBxIpA.Size = new System.Drawing.Size(2081, 895);
             this.txtBxIpA.TabIndex = 0;
             this.txtBxIpA.Text = "Drawers";
             this.txtBxIpA.UseVisualStyleBackColor = true;
@@ -70,81 +81,184 @@
             this.GrpDrawer.Controls.Add(this.GrpDrawerC);
             this.GrpDrawer.Controls.Add(this.GrpDrawerB);
             this.GrpDrawer.Controls.Add(this.GrpDrawerA);
-            this.GrpDrawer.Location = new System.Drawing.Point(6, 6);
+            this.GrpDrawer.Location = new System.Drawing.Point(8, 8);
+            this.GrpDrawer.Margin = new System.Windows.Forms.Padding(4);
             this.GrpDrawer.Name = "GrpDrawer";
-            this.GrpDrawer.Size = new System.Drawing.Size(1468, 665);
+            this.GrpDrawer.Padding = new System.Windows.Forms.Padding(4);
+            this.GrpDrawer.Size = new System.Drawing.Size(1957, 831);
             this.GrpDrawer.TabIndex = 1;
             this.GrpDrawer.TabStop = false;
             this.GrpDrawer.Text = "Drawers";
             // 
             // GrpDrawerD
             // 
-            this.GrpDrawerD.Location = new System.Drawing.Point(1210, 375);
+            this.GrpDrawerD.Location = new System.Drawing.Point(1613, 469);
+            this.GrpDrawerD.Margin = new System.Windows.Forms.Padding(4);
             this.GrpDrawerD.Name = "GrpDrawerD";
-            this.GrpDrawerD.Size = new System.Drawing.Size(233, 265);
+            this.GrpDrawerD.Padding = new System.Windows.Forms.Padding(4);
+            this.GrpDrawerD.Size = new System.Drawing.Size(311, 331);
             this.GrpDrawerD.TabIndex = 3;
             this.GrpDrawerD.TabStop = false;
             this.GrpDrawerD.Text = "D(192.168.0.54)";
             // 
             // GrpDrawerC
             // 
-            this.GrpDrawerC.Location = new System.Drawing.Point(1124, 375);
+            this.GrpDrawerC.Location = new System.Drawing.Point(1499, 469);
+            this.GrpDrawerC.Margin = new System.Windows.Forms.Padding(4);
             this.GrpDrawerC.Name = "GrpDrawerC";
-            this.GrpDrawerC.Size = new System.Drawing.Size(69, 265);
+            this.GrpDrawerC.Padding = new System.Windows.Forms.Padding(4);
+            this.GrpDrawerC.Size = new System.Drawing.Size(92, 331);
             this.GrpDrawerC.TabIndex = 2;
             this.GrpDrawerC.TabStop = false;
             this.GrpDrawerC.Text = "C(192.168.0.50)";
             // 
             // GrpDrawerB
             // 
-            this.GrpDrawerB.Location = new System.Drawing.Point(1210, 21);
+            this.GrpDrawerB.Location = new System.Drawing.Point(1613, 26);
+            this.GrpDrawerB.Margin = new System.Windows.Forms.Padding(4);
             this.GrpDrawerB.Name = "GrpDrawerB";
-            this.GrpDrawerB.Size = new System.Drawing.Size(233, 265);
+            this.GrpDrawerB.Padding = new System.Windows.Forms.Padding(4);
+            this.GrpDrawerB.Size = new System.Drawing.Size(311, 331);
             this.GrpDrawerB.TabIndex = 1;
             this.GrpDrawerB.TabStop = false;
             this.GrpDrawerB.Text = "B(192.168.0.42)";
             // 
             // GrpDrawerA
             // 
-            this.GrpDrawerA.Location = new System.Drawing.Point(6, 21);
+            this.GrpDrawerA.Location = new System.Drawing.Point(8, 26);
+            this.GrpDrawerA.Margin = new System.Windows.Forms.Padding(4);
             this.GrpDrawerA.Name = "GrpDrawerA";
-            this.GrpDrawerA.Size = new System.Drawing.Size(854, 565);
+            this.GrpDrawerA.Padding = new System.Windows.Forms.Padding(4);
+            this.GrpDrawerA.Size = new System.Drawing.Size(1139, 706);
             this.GrpDrawerA.TabIndex = 0;
             this.GrpDrawerA.TabStop = false;
             this.GrpDrawerA.Text = "A(192.168.0.34)";
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(1559, 713);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(2081, 895);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Controls.Add(this.Lock);
+            this.tabPage1.Controls.Add(this.Unlock);
+            this.tabPage1.Controls.Add(this.BTGetOS);
+            this.tabPage1.Controls.Add(this.BTPutOS);
+            this.tabPage1.Controls.Add(this.lblBoxType);
+            this.tabPage1.Controls.Add(this.txtBoxType);
+            this.tabPage1.Controls.Add(this.BTPutDR_0204);
+            this.tabPage1.Controls.Add(this.BTGetDR_0204);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(1559, 713);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(2081, 895);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "BoxTransfer";
             this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // Lock
+            // 
+            this.Lock.Location = new System.Drawing.Point(583, 136);
+            this.Lock.Name = "Lock";
+            this.Lock.Size = new System.Drawing.Size(224, 47);
+            this.Lock.TabIndex = 7;
+            this.Lock.Text = "Close box && Lock";
+            this.Lock.UseVisualStyleBackColor = true;
+            this.Lock.Click += new System.EventHandler(this.Lock_Click);
+            // 
+            // Unlock
+            // 
+            this.Unlock.Location = new System.Drawing.Point(583, 69);
+            this.Unlock.Name = "Unlock";
+            this.Unlock.Size = new System.Drawing.Size(224, 48);
+            this.Unlock.TabIndex = 6;
+            this.Unlock.Text = "Unlock && Open box";
+            this.Unlock.UseVisualStyleBackColor = true;
+            this.Unlock.Click += new System.EventHandler(this.Unlock_Click);
+            // 
+            // BTGetOS
+            // 
+            this.BTGetOS.Location = new System.Drawing.Point(301, 136);
+            this.BTGetOS.Name = "BTGetOS";
+            this.BTGetOS.Size = new System.Drawing.Size(220, 48);
+            this.BTGetOS.TabIndex = 5;
+            this.BTGetOS.Text = "BT get box from OS";
+            this.BTGetOS.UseVisualStyleBackColor = true;
+            this.BTGetOS.Click += new System.EventHandler(this.BTGetOS_Click);
+            // 
+            // BTPutOS
+            // 
+            this.BTPutOS.Location = new System.Drawing.Point(301, 69);
+            this.BTPutOS.Name = "BTPutOS";
+            this.BTPutOS.Size = new System.Drawing.Size(220, 49);
+            this.BTPutOS.TabIndex = 4;
+            this.BTPutOS.Text = "BT put box to OS";
+            this.BTPutOS.UseVisualStyleBackColor = true;
+            this.BTPutOS.Click += new System.EventHandler(this.BTPutOS_Click);
+            // 
+            // lblBoxType
+            // 
+            this.lblBoxType.AutoSize = true;
+            this.lblBoxType.Location = new System.Drawing.Point(30, 30);
+            this.lblBoxType.Name = "lblBoxType";
+            this.lblBoxType.Size = new System.Drawing.Size(73, 15);
+            this.lblBoxType.TabIndex = 3;
+            this.lblBoxType.Text = "Box type：";
+            // 
+            // txtBoxType
+            // 
+            this.txtBoxType.Location = new System.Drawing.Point(105, 25);
+            this.txtBoxType.Name = "txtBoxType";
+            this.txtBoxType.Size = new System.Drawing.Size(128, 25);
+            this.txtBoxType.TabIndex = 2;
+            this.txtBoxType.Text = "1:鐵盒; 2:水晶盒";
+            this.txtBoxType.Click += new System.EventHandler(this.txtBoxType_Click);
+            this.txtBoxType.TextChanged += new System.EventHandler(this.txtBoxType_TextChanged);
+            // 
+            // BTPutDR_0204
+            // 
+            this.BTPutDR_0204.Location = new System.Drawing.Point(21, 136);
+            this.BTPutDR_0204.Name = "BTPutDR_0204";
+            this.BTPutDR_0204.Size = new System.Drawing.Size(212, 48);
+            this.BTPutDR_0204.TabIndex = 1;
+            this.BTPutDR_0204.Text = "BT put to drawer_02_04";
+            this.BTPutDR_0204.UseVisualStyleBackColor = true;
+            this.BTPutDR_0204.Click += new System.EventHandler(this.BTPutDR_0204_Click);
+            // 
+            // BTGetDR_0204
+            // 
+            this.BTGetDR_0204.Location = new System.Drawing.Point(21, 69);
+            this.BTGetDR_0204.Name = "BTGetDR_0204";
+            this.BTGetDR_0204.Size = new System.Drawing.Size(212, 50);
+            this.BTGetDR_0204.TabIndex = 0;
+            this.BTGetDR_0204.Text = "BT get from drawer_02_04";
+            this.BTGetDR_0204.UseVisualStyleBackColor = true;
+            this.BTGetDR_0204.Click += new System.EventHandler(this.BTGetDR_0204_Click);
             // 
             // FrmTestUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1443, 763);
+            this.ClientSize = new System.Drawing.Size(1924, 954);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmTestUI";
             this.Text = "FrmTestUI";
             this.Load += new System.EventHandler(this.FrmTestUI_Load);
             this.tabControl1.ResumeLayout(false);
             this.txtBxIpA.ResumeLayout(false);
             this.GrpDrawer.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -160,5 +274,13 @@
         private System.Windows.Forms.GroupBox GrpDrawerA;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button BTPutDR_0204;
+        private System.Windows.Forms.Button BTGetDR_0204;
+        private System.Windows.Forms.Label lblBoxType;
+        private System.Windows.Forms.TextBox txtBoxType;
+        private System.Windows.Forms.Button Lock;
+        private System.Windows.Forms.Button Unlock;
+        private System.Windows.Forms.Button BTGetOS;
+        private System.Windows.Forms.Button BTPutOS;
     }
 }
