6bb975210f3d17b2de739e2febcd18aeb018cb63
LingChengYeh <a1qaz123698745@gmail.com>
2020-07-07 15:27:53 +0800

    ttt

--- vssol/MvAssistant.Mac.VerifyEqp/FrmTestUI.Designer.cs
+++ vssol/MvAssistant.Mac.VerifyEqp/FrmTestUI.Designer.cs
@@ -29,17 +29,19 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.txtBxIpA = new System.Windows.Forms.TabPage();
             this.GrpDrawer = new System.Windows.Forms.GroupBox();
             this.GrpDrawerD = new System.Windows.Forms.GroupBox();
             this.GrpDrawerC = new System.Windows.Forms.GroupBox();
             this.GrpDrawerB = new System.Windows.Forms.GroupBox();
             this.GrpDrawerA = new System.Windows.Forms.GroupBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.txtBxIpA.SuspendLayout();
             this.GrpDrawer.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,29 +49,21 @@
             this.tabControl1.Controls.Add(this.txtBxIpA);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(16, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1567, 739);
+            this.tabControl1.Size = new System.Drawing.Size(2089, 924);
             this.tabControl1.TabIndex = 4;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1559, 713);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // txtBxIpA
             // 
             this.txtBxIpA.Controls.Add(this.GrpDrawer);
-            this.txtBxIpA.Location = new System.Drawing.Point(4, 22);
+            this.txtBxIpA.Location = new System.Drawing.Point(4, 25);
+            this.txtBxIpA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBxIpA.Name = "txtBxIpA";
-            this.txtBxIpA.Padding = new System.Windows.Forms.Padding(3);
-            this.txtBxIpA.Size = new System.Drawing.Size(1559, 713);
+            this.txtBxIpA.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBxIpA.Size = new System.Drawing.Size(2081, 895);
             this.txtBxIpA.TabIndex = 0;
             this.txtBxIpA.Text = "Drawers";
             this.txtBxIpA.UseVisualStyleBackColor = true;
@@ -80,71 +74,105 @@
             this.GrpDrawer.Controls.Add(this.GrpDrawerC);
             this.GrpDrawer.Controls.Add(this.GrpDrawerB);
             this.GrpDrawer.Controls.Add(this.GrpDrawerA);
-            this.GrpDrawer.Location = new System.Drawing.Point(6, 6);
+            this.GrpDrawer.Location = new System.Drawing.Point(8, 8);
+            this.GrpDrawer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GrpDrawer.Name = "GrpDrawer";
-            this.GrpDrawer.Size = new System.Drawing.Size(1468, 665);
+            this.GrpDrawer.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GrpDrawer.Size = new System.Drawing.Size(1957, 831);
             this.GrpDrawer.TabIndex = 1;
             this.GrpDrawer.TabStop = false;
             this.GrpDrawer.Text = "Drawers";
             // 
             // GrpDrawerD
             // 
-            this.GrpDrawerD.Location = new System.Drawing.Point(1210, 375);
+            this.GrpDrawerD.Location = new System.Drawing.Point(1613, 469);
+            this.GrpDrawerD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GrpDrawerD.Name = "GrpDrawerD";
-            this.GrpDrawerD.Size = new System.Drawing.Size(233, 265);
+            this.GrpDrawerD.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GrpDrawerD.Size = new System.Drawing.Size(311, 331);
             this.GrpDrawerD.TabIndex = 3;
             this.GrpDrawerD.TabStop = false;
             this.GrpDrawerD.Text = "D(192.168.0.54)";
             // 
             // GrpDrawerC
             // 
-            this.GrpDrawerC.Location = new System.Drawing.Point(1124, 375);
+            this.GrpDrawerC.Location = new System.Drawing.Point(1499, 469);
+            this.GrpDrawerC.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GrpDrawerC.Name = "GrpDrawerC";
-            this.GrpDrawerC.Size = new System.Drawing.Size(69, 265);
+            this.GrpDrawerC.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GrpDrawerC.Size = new System.Drawing.Size(92, 331);
             this.GrpDrawerC.TabIndex = 2;
             this.GrpDrawerC.TabStop = false;
             this.GrpDrawerC.Text = "C(192.168.0.50)";
             // 
             // GrpDrawerB
             // 
-            this.GrpDrawerB.Location = new System.Drawing.Point(1210, 21);
+            this.GrpDrawerB.Location = new System.Drawing.Point(1613, 26);
+            this.GrpDrawerB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GrpDrawerB.Name = "GrpDrawerB";
-            this.GrpDrawerB.Size = new System.Drawing.Size(233, 265);
+            this.GrpDrawerB.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GrpDrawerB.Size = new System.Drawing.Size(311, 331);
             this.GrpDrawerB.TabIndex = 1;
             this.GrpDrawerB.TabStop = false;
             this.GrpDrawerB.Text = "B(192.168.0.42)";
             // 
             // GrpDrawerA
             // 
-            this.GrpDrawerA.Location = new System.Drawing.Point(6, 21);
+            this.GrpDrawerA.Location = new System.Drawing.Point(8, 26);
+            this.GrpDrawerA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GrpDrawerA.Name = "GrpDrawerA";
-            this.GrpDrawerA.Size = new System.Drawing.Size(854, 565);
+            this.GrpDrawerA.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GrpDrawerA.Size = new System.Drawing.Size(1139, 706);
             this.GrpDrawerA.TabIndex = 0;
             this.GrpDrawerA.TabStop = false;
             this.GrpDrawerA.Text = "A(192.168.0.34)";
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(2081, 895);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1559, 713);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(2081, 895);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "BoxTransfer";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FrmTestUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1621, 763);
+            this.ClientSize = new System.Drawing.Size(1924, 954);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmTestUI";
             this.Text = "FrmTestUI";
             this.Load += new System.EventHandler(this.FrmTestUI_Load);
             this.tabControl1.ResumeLayout(false);
             this.txtBxIpA.ResumeLayout(false);
             this.GrpDrawer.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -160,5 +188,6 @@
         private System.Windows.Forms.GroupBox GrpDrawerA;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
     }
 }
