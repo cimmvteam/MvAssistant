35c47805e46d695239d82ecaecd07cd0d1f99e73
King Liu <ntutking@hotmail.com>
2020-08-18 09:48:21 +0800

    TriggerMember Note

--- vssol/MaskAutoCleaner.v1_0/StateMachineBeta/TriggerMemberAsync.cs
+++ vssol/MaskAutoCleaner.v1_0/StateMachineBeta/TriggerMemberAsync.cs
@@ -10,6 +10,11 @@ namespace MaskAutoCleaner.v1_0.StateMachineBeta
     /// <summary>Transition Member for TriggerAsync</summary>
     public class TriggerMemberAsync : TriggerMemberBase
     {
+        /// <summary>TriggerAsync 的 Guard</summary>
+        /// <remarks>
+        /// parameter: DateTime, 開始時間, 計算逾時用,
+        /// return: bool
+        /// </remarks>
         public Func<DateTime, bool> Guard { get; set; }
     }
 }
--- vssol/MaskAutoCleaner.v1_0/StateMachineBeta/TriggerMemberBase.cs
+++ vssol/MaskAutoCleaner.v1_0/StateMachineBeta/TriggerMemberBase.cs
@@ -9,10 +9,15 @@ namespace MaskAutoCleaner.v1_0.StateMachineBeta
     /// <summary>Base Class of TriggerMember & TriggerMemberAsync</summary>
     public abstract class TriggerMemberBase
     {
+        /// <summary>Trigger 的Action</summary>
         public Action<object> Action { get; set; }
+        /// <summary>Action 的參數,不需參數則設為 null</summary>
         public object ActionParameter { get; set; }
+        /// <summary>例外的處理函式</summary>
         public Action<MacState, Exception> ExceptionHandler { get; set; }
+        /// <summary>Entry的參數</summary>
         public MacStateEntryEventArgs NextStateEntryEventArgs { get; set; }
+        /// <summary>Exit 的參數</summary>
         public MacStateExitEventArgs ThisStateExitEventArgs { get; set; }
 
     }
