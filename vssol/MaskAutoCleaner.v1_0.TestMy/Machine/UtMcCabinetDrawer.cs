330bb84b8dccce552936bb605dbbc10c2c4a4de6
King Liu <ntutking@hotmail.com>
2020-10-19 16:44:13 +0800

    OK

--- vssol/MaskAutoCleaner.v1_0.TestMy/Machine/UtMcCabinetDrawer.cs
+++ vssol/MaskAutoCleaner.v1_0.TestMy/Machine/UtMcCabinetDrawer.cs
@@ -234,7 +234,7 @@ namespace MaskAutoCleaner.v1_0.TestMy.Machine
 
         /// <summary>測試 MoveTrayToHomeWaitingLoadInstruction() 指令</summary>
         /// <remarks>
-        /// 
+        /// 2020/10/19 OK
         /// </remarks>
         [TestMethod]
        public void MoveTrayToHomeWaitingLoadInstruction()
