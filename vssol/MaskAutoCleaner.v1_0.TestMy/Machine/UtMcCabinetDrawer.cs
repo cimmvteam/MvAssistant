c3da933f5e3ce429ec8dfa788bb6a582735c84dd
King Liu <ntutking@hotmail.com>
2020-10-28 14:59:21 +0800

    Drawer State Machine Fake Test; Comman: Load_MoveTrayToIn()

--- vssol/MaskAutoCleaner.v1_0.TestMy/Machine/UtMcCabinetDrawer.cs
+++ vssol/MaskAutoCleaner.v1_0.TestMy/Machine/UtMcCabinetDrawer.cs
@@ -165,13 +165,13 @@ namespace MaskAutoCleaner.v1_0.TestMy.Machine
         public void Load_MoveTrayToIn(/*EnumMachineID machineID*/)
         {
             // DataRow
-            EnumMachineID machineID = EnumMachineID.MID_DRAWER_01_01;
+            EnumMachineID machineID1 = EnumMachineID.MID_DRAWER_01_01;
+            EnumMachineID machineID2 = EnumMachineID.MID_DRAWER_01_02;
+            var machine1 = MacMsCabinet.GetMacMsCabinetDrawer(machineID1, DicStateMachines);
+            var machine2 = MacMsCabinet.GetMacMsCabinetDrawer(machineID2, DicStateMachines);
 
-            var machine = MacMsCabinet.GetMacMsCabinetDrawer(machineID, DicStateMachines);
-
-          
-
-            machine.Load_MoveTrayToIn();
+            machine1.Load_MoveTrayToIn();
+            machine2.Load_MoveTrayToIn();
 
             Repeat();
         }
