a09b70b586a2977a26b91fb225207dd5aba43d3f
LingChengYeh <a1qaz123698745@gmail.com>
2020-11-12 16:13:36 +0800

    Hirata scenario test class add

---
+++ vssol/MaskTool.TestMy/MachineRealHal/Hirata_ScenrioTest/Ut024_OS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvAssistant.Mac.TestMy.MachineRealHal.Hirata_ScenrioTest
+{
+    [TestClass]
+    public class Ut024_OS
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
